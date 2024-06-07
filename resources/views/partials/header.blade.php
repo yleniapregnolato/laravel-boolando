f0fb6a01f2e2f5a1ab83aa26c0ca2e04c29b3cca
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-07 16:42:56 +0200

    fix: header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 <header>
     <div class="row justify-content-between">
-        <div class="col-2">
+        <div class="col-2 mt-3">
             <ul class="d-flex justify-content-between">
                 <li><a href="">Donna</a></li>
                 <li><a href="">Uomo</a></li>
                 <li><a href="">Bambino</a></li>
             </ul>
         </div>
-        <div class="col-3">
-            <img class="img-fluid" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="boolean-logo">
+        <div class="col-3 mt-3">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="boolean-logo">  
         </div>
         <div class="col-2">
 
