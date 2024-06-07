deab506cf34f6721ed479356698c460bfbe1c65e
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-07 16:32:57 +0200

    feat: header with logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,19 @@
 <header>
-    <ul>
-        <li><a href="">Donna</a></li>
-        <li><a href="">Uomo</a></li>
-        <li><a href="">Bambino</a></li>
-    </ul>
+    <div class="row justify-content-between">
+        <div class="col-2">
+            <ul class="d-flex justify-content-between">
+                <li><a href="">Donna</a></li>
+                <li><a href="">Uomo</a></li>
+                <li><a href="">Bambino</a></li>
+            </ul>
+        </div>
+        <div class="col-3">
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="boolean-logo">
+        </div>
+        <div class="col-2">
+
+        </div>
+    </div>
+
+
 </header>
