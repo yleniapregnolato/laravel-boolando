4f915cfef390e255ec567b051c0c0ec5cf6a545d
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-08 11:48:05 +0200

    feat: brand and name in card.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5 mb-5">
         <div class="row">
             @foreach ($products as $product)
-                <div class="col-4">
+                <div class="col-4 mb-5">
                     @include('partials.card')
                 </div>
             @endforeach
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,5 +1,7 @@
-<div class="card">
-    <div class="">
+<div class="card rounded-0 border-0">
+    <div>
         <img class="img-fluid" src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" alt="front-image">
+        <p class="m-0">{{ $product['brand'] }}</p>
+        <p class="fw-bold">{{ $product['name'] }}</p>  
     </div>   
 </div>
